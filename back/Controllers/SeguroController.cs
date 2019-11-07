5ee6f4c64fa7e733d189d86c9562751798199fbc
ulc <ulisses.cavalcante@altitude.com>
2019-11-06 21:09:16 -0300

    add local mockup

--- back/Controllers/SeguroController.cs
+++ back/Controllers/SeguroController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using back.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -11,7 +12,25 @@ namespace back.Controllers
         [HttpGet]
         public IEnumerable<Seguro> Get()
         {
-            return null;
+            List<Seguro> mockup = new List<Seguro> {
+                new Seguro {
+                     Id = 1,
+                    Nome = "Veiculo Passeio Nacional",
+                    Abrangencia = AbrangenciaEnum.Nacional,
+                    DtContratacao = DateTime.Now,
+                    VigenciaLimite = DateTime.Now.AddMonths(12)
+                },
+                new Seguro { 
+                    Id = 2,
+                    Nome = "Motorista de Aplicativo Estadual",
+                    Abrangencia = AbrangenciaEnum.Estadual,
+                    DtContratacao = DateTime.Now,
+                    VigenciaLimite = DateTime.Now.AddMonths(6)
+                }
+            };
+
+
+            return mockup;
         }
         [HttpPost]
         public IActionResult Post(Seguro seguro)
